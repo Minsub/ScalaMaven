d1b40ea4f7b8f3ec19f35d5b21539b41c890368e
Minsub Ji <jiminsub@gmail.com>
2018-01-01 19:31:08 +0900

    add study file

---
+++ src/main/scala/com/minsub/study/basic/Basic1.scala
@@ -0,0 +1,10 @@
+package com.minsub.study.basic
+
+/**
+  * Created by jiminsub on 2018. 1. 1..
+  */
+object Basic1 extends App {
+
+  println("START")
+
+}
---
+++ src/main/scala/com/minsub/study/collections/Collection1.scala
@@ -0,0 +1,10 @@
+package com.minsub.study.collections
+
+/**
+  * Created by jiminsub on 2018. 1. 1..
+  */
+object Collection1 extends App {
+
+
+
+}
